b1a19eedb8f7234ed88d7ccc7f8f148346867741
Michael Aponte <90635272+MichaelAponte4@users.noreply.github.com>
2023-02-09 16:50:24 +0000

    inicio del codigo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+  
+  cout<<"-----------𝘽𝙞𝙚𝙣𝙫𝙚𝙣𝙞𝙙𝙤𝙨 𝙖𝙡 𝙨𝙞𝙨𝙩𝙚𝙢𝙖 𝙖𝙘𝙖𝙙𝙚́𝙢𝙞𝙘𝙤-----------\n\n";
+  here:
+  int cestudiante=0;
+  cout<<"ingrese la cantidad de alumnos(estan permitidos un maximo de 99 alumnos):";
+  cin>>cestudiante;
+  if((cestudiante>0)&&(cestudiante<100)){
+    //base de datos de los estudiantes
+    int codigo=0;
+    //solicita datos del estudiante
+    for(int i=1;i<=cestudiante;i++){
+      cout<<"ingrese el codigo del estudiante "<<i;
+      cin>>codigo;
+    
+    }
+  }else{
+    goto here;
+  }
+  
 }
